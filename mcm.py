614e113b300932d7fe7b9abcf4063525b484b305
imarranz <ibon.martinez@imarranz.com>
2022-12-20 10:11:41 +0100

    :zap: update: general update, pylint conventions and warnings fixed:

--- mcm.py
+++ mcm.py
@@ -1,7 +1,9 @@
+"""mcm"""
+import pandas as pd
 def mcm(tn, fp, fn, tp):
     """Let be a confusion matrix like this:
-    
-    
+
+
       N    P
     +----+----+
     |    |    |
@@ -12,10 +14,12 @@ def mcm(tn, fp, fn, tp):
     | FN | TP |
     |    |    |
     +----+----+
-    
-    The observed values by columns and the expected values by rows and the positive class in right column. With these definitions, the TN, FP, FN and TP values are that order.
-    
-    
+
+    The observed values by columns and the expected values
+    by rows and the positive class in right column.
+    With these definitions, the TN, FP, FN and TP values are that order.
+
+
     Parameters
     ----------
     TN : integer
@@ -26,54 +30,55 @@ def mcm(tn, fp, fn, tp):
          False Negative
     TP : integer
          True Positive
-   
+
     Returns
     -------
     sum : DataFrame
           DataFrame with several metrics
-    
+
     Notes
     -----
     https://en.wikipedia.org/wiki/Confusion_matrix
     https://developer.lsst.io/python/numpydoc.html
-    
+
     Examples
     --------
     data = pd.DataFrame({
     'y_true': ['Positive']*47 + ['Negative']*18,
     'y_pred': ['Positive']*37 + ['Negative']*10 + ['Positive']*5 + ['Negative']*13})
-    
-    tn, fp, fn, tp = confusion_matrix(y_true = data.y_true, 
-                                  y_pred = data.y_pred, 
-                                  labels = ['Negative', 'Positive']).ravel()
-    
+
+    tn, fp, fn, tp = confusion_matrix(y_true = data.y_true,
+                                  y_pred = data.y_pred,
+                                  labels = ['Negative',
+                                  'Positive']).ravel()
+
     """
-    mcm = []
-    
-    mcm.append(['Sensitivity', tp / (tp + fn)])
-    mcm.append(['Recall', tp / (tp + fn)])
-    mcm.append(['True Positive rate (TPR)', tp / (tp + fn)])
-    mcm.append(['Specificity', tn / (tn + fp)])
-    mcm.append(['True Negative Rate (TNR)', tn / (tn + fp)])
-    
-    mcm.append(['Precision', tp / (tp + fp)])
-    mcm.append(['Positive Predictive Value (PPV)', tp / (tp + fp)])
-    mcm.append(['Negative Predictive Value (NPV)', tn / (tn + fn)])
-        
-    mcm.append(['False Negative Rate (FNR)', fn / (fn + tp)])
-    mcm.append(['False Positive Rate (FPR)', fp / (fp + tn)])
-    mcm.append(['False Discovery Rate (FDR)', fp / (fp + tp)])
-    
-    mcm.append(['Accuracy', (tp + tn) / (tp + tn + fp + fn)])
-    mcm.append(['F1 Score', 2*tp / (2*tp + fp + fn)])
-    
+    df_mcm = []
+
+    df_mcm.append(['Sensitivity', tp / (tp + fn)])
+    df_mcm.append(['Recall', tp / (tp + fn)])
+    df_mcm.append(['True Positive rate (TPR)', tp / (tp + fn)])
+    df_mcm.append(['Specificity', tn / (tn + fp)])
+    df_mcm.append(['True Negative Rate (TNR)', tn / (tn + fp)])
+
+    df_mcm.append(['Precision', tp / (tp + fp)])
+    df_mcm.append(['Positive Predictive Value (PPV)', tp / (tp + fp)])
+    df_mcm.append(['Negative Predictive Value (NPV)', tn / (tn + fn)])
+
+    df_mcm.append(['False Negative Rate (FNR)', fn / (fn + tp)])
+    df_mcm.append(['False Positive Rate (FPR)', fp / (fp + tn)])
+    df_mcm.append(['False Discovery Rate (FDR)', fp / (fp + tp)])
+
+    df_mcm.append(['Accuracy', (tp + tn) / (tp + tn + fp + fn)])
+    df_mcm.append(['F1 Score', 2*tp / (2*tp + fp + fn)])
+
     tpr = tp / (tp + fn)
     fpr = fp / (fp + tn)
-    
+
     fnr = fn / (fn + tp)
     tnr = tn / (tn + fp)
-    
-    mcm.append(['Positive Likelihood Ratio (LR+)', tpr / fpr])
-    mcm.append(['Negative Likelihood Ratio (LR-)', fnr / tnr])
-    
-    return pd.DataFrame(mcm, columns = ['Metric', 'Value'])
+
+    df_mcm.append(['Positive Likelihood Ratio (LR+)', tpr / fpr])
+    df_mcm.append(['Negative Likelihood Ratio (LR-)', fnr / tnr])
+
+    return pd.DataFrame(df_mcm, columns = ['Metric', 'Value'])
